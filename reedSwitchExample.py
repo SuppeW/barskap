e4b826187f3a7ff2baeab397ee002a24534d66a5
SuppeW <anders.woien96@gmail.com>
2019-01-31 19:19:51 +0100

    gjorde ferdig reedswitch eksempel

--- reedSwitchExample.py
+++ reedSwitchExample.py
@@ -1,8 +1,13 @@
 import RPi.GPIO as GPIO
 import time
 import espeak
+import pygame
 
 reedSwitchPin = 11
+choirsound = "heavenly_choir.mp3"
+
+pygame.mixer.init()
+pygame.mixer.music.load(choirsound)
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) #Hvilke nummer system du bruker til pins.
@@ -11,5 +16,9 @@ GPIO.setup(reedSwitchPin, GPIO.IN)
 while True:
     if i == 0:
         print("Door Closed")
+        ans = input("play sound: y or n: ")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
     else:
         print("Door Opened")
